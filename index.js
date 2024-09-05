84b0a3794326fc050ae9341ff4f7299f887b9b50
Isaac Svedin <swedinisaac@gmail.com>
2024-09-05 15:42:57 +0200

    version 1.0.2 - schema bot - made code more compact and added comments

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const client = new Discord.Client({
 });
 
 client.on('ready', async () => {
-    console.log(`Client has been initiated! ${client.user.username}`)
+    console.log(`Boten har startats! ${client.user.username}`)
 });
 
 
@@ -31,8 +31,7 @@ client.on('messageCreate', async (message) => {
                 })
             )
             .on("data", function (row) {
-                //Lägger in schemadatan per rad från schedule.csv filen.
-                data.push(row);
+                data.push(row); //<---------------Lägger in schemadatan per rad från schedule.csv filen.
             })
             .on("error", function (error) {
                 console.log(error.message);
@@ -42,33 +41,45 @@ client.on('messageCreate', async (message) => {
                 console.log("parsed csv data:");
                 console.log(data);
                 */
+                /*
+                * Deklarerar dates som ska hålla de exakta datum och tiderna för nästa lektion.
+                * For-loopen går igenom varje rad i data-arrayen gör dem till strängar och 
+                * återskapar mellanslag och tar bort andra tecken.
+                * Därefter tar ut år, månad, dag, timmar och minuter.
+                *
+                */
                 const dates = [];
-
-                let arrayLength = data.length;
-                for (let i = 0; i < arrayLength; i++) {
+                for (let i = 0; i < data.length; i++) {
                     let tmp = stringify(data[i]);
+
+                    //Datum, år, månad och dag:
                     let onlydate = tmp.split('Date=').pop().split('Time=')[0].replace('%20', ' ').replace('&', ' ').replace(/\D/g,'');
                     let year = onlydate.substring(0, 4);
                     let month = Number(onlydate.substring(4, 6))-1;
                     let day = onlydate.substring(6, 8);
 
+                    //Tid, timmar och minuter:
                     let onlytime = tmp.split('Time=').pop().split('Course')[0].replace('%20', ' ').replace('%20', ' ').replace('&', ' ').replace('%3A', ';').replace('%3A', ';');
-                    
                     let hours = onlytime.substring(0, 2);
-                    
                     let minutes = onlytime.substring(3, 5);
                     
-                    let fulldate = new Date(year, month, day, hours, minutes)
-                    //Lägger in exakta tidsvärden:
-                    dates.push(fulldate);
+                    //Deklarerar schemats datum som Date-objekt och lägger in:
+                    dates.push(new Date(year, month, day, hours, minutes)); 
                     
                 }
-                let currentdate = new Date(); //Tidpunkt nu.
-                let index = 0;
+                /*
+                * currentdate är den exakta nupunkten.
+                * index hjälper oss att loopa indexeringsvärden i arrayen dates.
+                * text deklareras utanför do-while-loopen för att ge räckvidd i loopen 
+                * där den tilldelas ett värde och till message.reply där den 
+                * konkateneras med utskriftsmeddelandet.
+                */
+                let currentdate = new Date();
+                let index = 0; 
                 let text = "";
                 do{
-                    console.log(dates[index] + " " + currentdate)
-                    text = Math.floor((dates[index]).getTime()/1000).toFixed(0);
+                    console.log(dates[index] + " " + currentdate);
+                    text = Math.floor((dates[index]).getTime()/1000).toFixed(0); //<---------Översätter till unixtime.
 
                 }while((currentdate.getTime() - dates[index++].getTime()) > 0);
 
